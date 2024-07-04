f2b85d47c337fd9fbfaca27bfe5866583058662a
VonIsaac <119748258+VonIsaac@users.noreply.github.com>
2024-07-05 00:36:22 +0800

    Adding Items in UI

--- src/Components/Sales-Components/EventSales.jsx
+++ src/Components/Sales-Components/EventSales.jsx
@@ -1,17 +1,48 @@
-
-
+import { Link } from "react-router-dom"
+import { ItemData } from "../../Sales-ItemData/Data"
+import { currencyFormatter } from "../../Util/formattert"
 const EventSales = () => {
     return(
-        <div className=" flex flex-col my-5 justify-center items-center">
-            <h1 className=" text-[85px] font-serif font-bold leading-tight">All Items!</h1>
-            <p className=" font-semibold text-xl font-serif ">
-                All Items are sale up to <span className=" text-green-600">20%,</span> Get your's now!
-            </p>
+       <>
+            <div className=" flex flex-col mt-4 mb-9 justify-center items-center">
+                <h1 className=" text-[85px] font-serif font-bold leading-tight">All Items!</h1>
+                <p className=" font-semibold text-xl font-serif ">
+                    All Items are sale up to <span className=" text-green-600">20%,</span> Get your's now!
+                </p>
 
-            
-        </div>
+                
+            </div>
+       
+            <div className=" mx-2">
+                <ul className=" grid grid-rows-4 grid-flow-col gap-5 ">
+                    {ItemData.map((item, key) => (
+                        <li key={key.id} className=" border bg-gray-900 text-stone-200 rounded-lg">
+                            <Link className=" flex justify-center items-center mt-5">
+                                <img src={item.img} alt={item.description} className=" rounded-md w-[18rem]"/>
+                            </Link>
+                            <div className=" mx-1  ">
+                                <h1 className=" text-3xl font-bold my-2">{item.name}</h1>
+                                <p className=" font-semibold leading-snug">{currencyFormatter.format(item.price)}</p>
+
+                            </div>
+
+                            <div className=" flex flex-col justify-items-start items-start my-3">
+                                <button>Add to Cart</button>
+                                <button>Buy</button>
+                               
+                            </div>
+                        </li>
+                        
+                    ))}
+                </ul>
+
+            </div>
+       </>
+
+        
     )
 
 }
 
 export default EventSales
+
--- src/Sales-ItemData/Data.js
+++ src/Sales-ItemData/Data.js
@@ -10,11 +10,130 @@ import Data9 from '../img/Sales-Img/hmgoepprod (8).jpg'
 import Data10 from '../img/Sales-Img/hmgoepprod.jpg'
 import Data11 from '../img/Sales-Img/milwaukee-bucks-statement-edition-jordan-dri-fit-nba-swingman-jersey-WSK0WG.png'
 import Data12 from '../img/Sales-Img/p-6000-shoes-QcQbpx.png'
-import Data13 from '../img/Sales-Img/src/ph-11134207-7r98p-lrrpgju7mqtg24.jpg'
+import Data13 from '../img/Sales-Img/ph-11134207-7r98p-lrrpgju7mqtg24.jpg'
 import Data14 from '../img/Sales-Img/ph-11134207-7r991-lpekg5q870wf7f.jpg'
-import Data15 from '../img/Sales-Img/src/img/Sales-Img/zion-shorts-8TXb1C.png'
+import Data15 from '../img/Sales-Img/zion-shorts-8TXb1C.png'
 export const ItemData = [
     {
-        
-    }
+        id: 'it1',
+        name: 'Item 1',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data1
+    },
+
+    {
+        id: 'it2',
+        name: 'Item 2',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data2
+    },
+
+    {
+        id: 'it3',
+        name: 'Item 3',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data3
+    },
+
+    {
+        id: 'it4',
+        name: 'Item 4',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data4
+    },
+
+    {
+        id: 'it5',
+        name: 'Item 5',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data5
+    },
+
+    {
+        id: 'it6',
+        name: 'Item 6',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data6
+    },
+
+    {
+        id: 'it7',
+        name: 'Item 7',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data7
+    },
+
+    {
+        id: 'it8',
+        name: 'Item 8',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data8
+    },
+
+    {
+        id: 'it9',
+        name: 'Item 9',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data9
+    },
+
+    {
+        id: 'it10',
+        name: 'Item 10',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data10
+    },
+
+    {
+        id: 'it11',
+        name: 'Item 11',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data11
+    },
+
+    {
+        id: 'it12',
+        name: 'Item 12',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data12
+    },
+
+    {
+        id: 'it13',
+        name: 'Item 13',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data13
+    },
+
+    {
+        id: 'it14',
+        name: 'Item 14',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data14
+    },
+
+    {
+        id: 'it15',
+        name: 'Item 15',
+        price: '100',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, necessitatibus.',
+        img: Data15
+    },
+
+   
+
 ]
