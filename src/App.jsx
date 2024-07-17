59d2475349e3b3ff90b09d8afcf7c5e2cc411c6e
VonIsaac <119748258+VonIsaac@users.noreply.github.com>
2024-07-18 02:05:57 +0800

    initialize fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { RouterProvider, createBrowserRouter} from "react-router-dom"
 import Events from "./Components/Events"
 import EventSales from "./Components/Sales-Components/EventSales"
 import EventDetails from "./Components/Sales-Components/EventDetails"
-import Modal from "./UI/Modal"
+import { QueryClientProvider } from "@tanstack/react-query";
+import {queryClient} from './Util/http'
 
 const router = createBrowserRouter([
   {
@@ -29,7 +30,10 @@ function App() {
 
   return (
     <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+    </QueryClientProvider>
+      
     </>
   )
 }
---
+++ src/Util/http.js
@@ -0,0 +1,22 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient();
+
+
+export async function handleBuyItem(buyItem){
+    const response = await fetch('https://ecomerce-web-app-default-rtdb.firebaseio.com/buy.json', {
+        method: 'POST',
+        body: JSON.stringify(buyItem),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    });
+
+    if(!response.ok){
+        throw new Error('An Eror Occured')
+    }
+
+    const resData = await response.json()
+
+    return resData
+}
